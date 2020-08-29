3b564ba2181095b683d1d948b1c133bf208005ad
vikascowdary47 <vikaschowdary47@gmail.com>
2020-08-29 15:41:14 +0530

    header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <h1>reactjs</h1>
+      <div className="app__body">
+        <Sidebar />
+      </div>
     </div>
   );
 }
