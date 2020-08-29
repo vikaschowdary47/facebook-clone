f8491fbcd2c200c37dd769862739c94465c9a48f
vikascowdary47 <vikaschowdary47@gmail.com>
2020-08-29 13:55:00 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAsXQ2vfPfyDP_v5oKWg0gx1AAEwr9_v8U",
+  authDomain: "fb-clone-47.firebaseapp.com",
+  databaseURL: "https://fb-clone-47.firebaseio.com",
+  projectId: "fb-clone-47",
+  storageBucket: "fb-clone-47.appspot.com",
+  messagingSenderId: "681767967393",
+  appId: "1:681767967393:web:ea9c80972c52c52b61aee0",
+  measurementId: "G-FT535MHS2G",
+};
