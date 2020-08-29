11316982e312da0d6037b5435d1f24ae18b11b3c
vikascowdary47 <vikaschowdary47@gmail.com>
2020-08-29 16:21:19 +0530

    sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,10 +1,24 @@
 import React from "react";
+import SidebarRow from "./SidebarRow";
 import "./Sidebar.css";
+import LocalHospitalIcon from "@material-ui/icons/LocalHospital";
+import EmojiFlagsIcon from "@material-ui/icons/EmojiFlags";
+import PeopleIcon from "@material-ui/icons/People";
+import StorefrontIcon from "@material-ui/icons/Storefront";
+import ChatIcon from "@material-ui/icons/Chat";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import ExpandMoreOutlinedIcon from "@material-ui/icons/ExpandMoreOutlined";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <h1>Sidebar</h1>
+      <SidebarRow src="lsls./ssf" title="vikas" />
+      <SidebarRow title="Pages" Icon={EmojiFlagsIcon} />
+      <SidebarRow title="Friends" Icon={PeopleIcon} />
+      <SidebarRow title="Messenger" Icon={ChatIcon} />
+      <SidebarRow title="Marketplace" Icon={StorefrontIcon} />
+      <SidebarRow title="Videos" Icon={VideoLibraryIcon} />
+      <SidebarRow title="Marketpla" Icon={ExpandMoreOutlinedIcon} />
     </div>
   );
 };
